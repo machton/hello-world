c46d1223d7b2cf0a2832de31b85ea953cea07018
machton <wade419@gmail.com>
2014-11-20 17:11:31 -0600

    Update testing_ground.py

--- testing_ground.py
+++ testing_ground.py
@@ -100,3 +100,22 @@ print '%s-%s-%s' % (now.year, now.month, now.day)
 print '%s/%s/%s' % (now.month, now.day, now.year)
 
 print '%s:%s:%s' % (now.hour, now.minute, now.second)
+
+
+# defining a program, comparing inputs, and using if-then
+
+def clinic():
+    print "You've just entered the clinic!"
+    print "Do you take the door on the left or the right?"
+    answer = raw_input("Type left or right and hit 'Enter'.").lower()
+    if answer == "left" or answer == "l":
+        print "This is the Verbal Abuse Room, you heap of parrot droppings!"
+    elif answer == "right" or answer == "r":
+        print "Of course this is the Argument Room, I've told you that already!"
+    else:
+        print "You didn't pick left or right! Try again."
+        clinic()
+
+clinic()
+
+
